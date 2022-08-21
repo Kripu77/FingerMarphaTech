5f5d9b0d89a24a6e342f979f2f6b73634ae3dd3f
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2022-08-21 16:01:34 +1000

    Head Compo

---
+++ marph/components/SEO/SEO.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import React from 'react'
+
+const SEO = () => {
+  return (
+   <Head>
+  <title>Welcome | Fingermarpha™ | Experience Faster, Digital Solutions</title>
+  <meta data-react-helmet="true" name="description" content="Empower your team to unlock game-changing improvements in operational efficiency, health and safety, and customer experience."/>
+  <meta data-react-helmet="true" property="image" content="https://cdn.sanity.io/images/wdfk0n2i/production/9de946860e488a6f9d3578c4593fcea19d637c4f-2880x2047.jpg"/>
+  <meta data-react-helmet="true" property="og:title" content="Welcome"/>
+  <meta data-react-helmet="true" property="og:description" content="Empower your team to unlock game-changing improvements in operational efficiency, health and safety, and customer experience."/>
+  <meta data-react-helmet="true" property="og:image" content="https://cdn.sanity.io/images/wdfk0n2i/production/9de946860e488a6f9d3578c4593fcea19d637c4f-2880x2047.jpg"/><meta data-react-helmet="true" property="og:type" content="website"/><meta data-react-helmet="true" name="twitter:card" content="summary"/><meta data-react-helmet="true" name="twitter:creator" content="@Kripu77"/><meta data-react-helmet="true" name="twitter:title" content="Welcome"/><meta data-react-helmet="true" name="twitter:description" content="Empower your team to unlock game-changing improvements in operational efficiency, health and safety, and customer experience."/>
+
+  
+   </Head>
+  )
+}
+
+export default SEO
--- marph/pages/index.js
+++ marph/pages/index.js
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import Panels from '../components/body/Panels'
 import Hero from '../components/Masthead/Hero'
 import Nav from '../components/Masthead/Nav'
+import SEO from '../components/SEO/SEO'
 import BodySlider from '../components/slider/BodySlider'
 
 
 export default function Home() {
   return (
    <main >
+    <SEO/>
   <Nav/>
   <Hero/>
   <Panels/>
