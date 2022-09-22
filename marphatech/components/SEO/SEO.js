ae156655df198e8dc426e2cfaf551659cdf4519f
Kripu Khadka <84505567+Kripu77@users.noreply.github.com>
2022-09-22 20:28:18 +1000

    SEO

--- marphatech/components/SEO/SEO.js
+++ marphatech/components/SEO/SEO.js
@@ -4,7 +4,7 @@ import React from 'react'
 const SEO = () => {
   return (
    <Head>
-  <title>Welcome | Mmarphatech™ | Experience Faster, Digital Solutions</title>
+  <title>Welcome | Marphatech™ | Experience Faster, Digital Solutions</title>
   <meta data-react-helmet="true" name="description" content="Empower your team to unlock game-changing improvements in operational efficiency, health and safety, and customer experience."/>
   <meta data-react-helmet="true" property="image" content="https://cdn.sanity.io/images/wdfk0n2i/production/9de946860e488a6f9d3578c4593fcea19d637c4f-2880x2047.jpg"/>
   <meta data-react-helmet="true" property="og:title" content="Welcome"/>
