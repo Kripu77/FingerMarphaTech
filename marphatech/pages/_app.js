cfff942ae16511ab75f93fc9355aaf80a9c0685c
Kripu Khadka <84505567+Kripu77@users.noreply.github.com>
2022-11-08 20:07:47 +1100

    app component

--- marphatech/pages/_app.js
+++ marphatech/pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import {ParallaxProvider} from "react-scroll-parallax";
 import Layout from '../components/layout/Layout';
 
-
+//main app component
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
